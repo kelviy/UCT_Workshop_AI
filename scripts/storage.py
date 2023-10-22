4b82dfab97495a816716e8a37dcd8cd27ea6b531
Kelvin Wei <52874397+kelviy@users.noreply.github.com>
2023-10-22 10:35:58 +0200

    Added missing code
    Was an empty file, added the code

--- scripts/storage.py
+++ scripts/storage.py
@@ -1,1 +1,15 @@
+import boto3
+from botocore.exceptions import NoCredentialsError
 
+def uploadToS3(file_name, bucket, s3_file_name):
+    s3 = boto3.client('s3')
+
+    s3.upload_file(file_name, bucket, s3_file_name)
+    print(f"Uploaded succesfully File upload as {s3_file_name}")
+    return True
+
+local_file_name = 'output.txt'
+bucket_name = 'uct-workshop' #name of bucket in aws s3
+s3_file_name = 'output.txt'
+
+uploadToS3(local_file_name, bucket_name, s3_file_name)
